32ca1c8e4908009db842be5fdd8a2cd648c559e9
xhdlphzr <xhdlphzr@163.com>
2024-04-01 22:27:21 +0800

    C++ Code Runner

---
+++ source/run.cpp
@@ -0,0 +1,28 @@
+#include <cstdio>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+int main (int argc, char *argv[]) 
+{
+  if (argc != 2)
+  {
+    printf("You can't do that!!!");
+
+    return 0;
+  }
+  
+  char path[114514] = {};
+  char run_code[114514] = {};
+  
+  strcpy(path, argv[1]);
+  sprintf(run_code, "g++ .\\source\\%s.cpp -o .\\build\\%s.exe -g -D DEBUG -std=c++14 && .\\build\\%s.exe", path, path, path);
+  
+  // printf("%s", run_code);
+  printf("Run now!!!");
+
+  system(run_code);
+
+  return 0;
+}

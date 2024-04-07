1eabda2c08e4c57d864864e7346f3c21bf94aa7e
xhdlphzr <xhdlphzr@163.com>
2024-04-07 21:20:29 +0800

    用法更新

--- Windows/include/cpp.h
+++ Windows/include/cpp.h
@@ -8,29 +8,29 @@ void run(char path[], char mode[])
   char build_code[] = {};
   char run_code[] = {};
 
-  sscanf(build_code, "g++ .\\source\\%s.cpp -o .\\build\\%s.exe -g -D DEBUG -O2 -static -std=c++14", path[1], path[1], path[1]);
+  sscanf(build_code, "g++ .\\%s.cpp -o .\\%s.exe -g -D DEBUG -O2 -static -std=c++14", path[1], path[2]);
 
   if (mode == "run")
   {
     system(build_code);
     
-    sscanf(run_code, ".\\build\\%s.exe", path[1]);
-    printf("%s.cpp Run Now !", path[1]);
+    sscanf(run_code, ".\\%s.exe", path[2]);
+    printf(".\\%s.cpp Run Now !", path[1]);
   }
 
   if (mode == "debug")
   {
     system(build_code);
 
-    sscanf(run_code, "gdb .\\build\\%s.exe", path[1]);
-    printf("%s.cpp Debug Now !", path[1]);
+    sscanf(run_code, "gdb .\\%s.exe", path[2]);
+    printf(".\\%s.cpp Debug Now !", path[1]);
   }
 
   if (mode == "build")
   {
     system(build_code);
 
-    printf("%s.cpp Debug Now !", path[1]);
+    printf(".\\%s.cpp Debug Now !", path[1]);
   }
 
   system(run_code);
--- Windows/source/debug.cpp
+++ Windows/source/debug.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char *argv[])
 {
-  if (argc != 2)
+  if (argc != 3)
   {
     printf("You Can't Do That !");
 
